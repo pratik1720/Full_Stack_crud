520f31f8a231efd5f8602d8782183ffa904ee63f
pratik khond <112702992+pratik1720@users.noreply.github.com>
2023-11-16 20:23:31 +0530

    FrontEnd

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
+import Navbar from './layout/navbar';
+import Home from './Pages/home' 
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import AddUser from './User/AddUser';
+import EditUser from './User/EditUser';
+import ViewUsers from './User/ViewUsers';
+function App() {
+  return (
+    <div className="App">
+    <Router>
+    <Navbar/>
+    <Routes>
+    
+    <Route exact path="/" element={<Home/>}/> 
+    <Route exact path="/adduser" element={<AddUser/>}/>  
+    <Route exact path="/edituser/:id" element={<EditUser/>}/>
+    <Route exact path="/viewuser/:id" element={<ViewUsers/>}/>  
+    </Routes>
+   
+     
+    
+    </Router>
+      
+    </div>
+  );
+}
+
+export default App;
